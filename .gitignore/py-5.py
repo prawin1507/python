12d0203875e68751f9263c22c485c1ffdfba0b3e
prawin1507 <36299129+prawin1507@users.noreply.github.com>
2018-05-21 13:53:22 +0530

    Create py-5.py

---
+++ .gitignore/py-5.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+c = int(input())
+if (a >= b) and (a >= c):
+   print("largest number is ",a)
+elif (b >= a) and (b >= c):
+   print("largest number is ",b)
+else:
+   print("largest number is ",c)
