db579a7704b25899db8557d00e332a2bfd5bf665
prawin1507 <36299129+prawin1507@users.noreply.github.com>
2018-05-21 11:38:58 +0530

    Create py-2.py

---
+++ .gitignore/py-2.py
@@ -0,0 +1,7 @@
+num = int(input())
+if num%2 !=0:
+   print("The given number is an odd number")
+elif num == 0:
+   print("The given number is zero")
+else:
+   print("The given number is an even number")
